c70abaf529d2cf929a63b15d4cf8741af313b873
justynize <nwobajusticeekene12@gmail.com>
2025-10-15 14:45:17 +0100

    updated

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-var countDownDate = new Date().getTime() + (30 * 24 * 60 * 60 * 1000);
 
-var x = setInterval(function() {
+let countDownDate;
+
+if (localStorage.getItem("countDownDate")) {
+    countDownDate = parseInt(localStorage.getItem("countDownDate"));
+} else {
+    // If not saved, set it to 30 days from now and save it
+    countDownDate = new Date().getTime() + (30 * 24 * 60 * 60 * 1000);
+    localStorage.setItem("countDownDate", countDownDate);
+}
+
+// Countdown Timer
+
+var x = setInterval(function () {
     var now = new Date().getTime();
     var distance = countDownDate - now;
 
@@ -14,10 +25,11 @@ var x = setInterval(function() {
     document.getElementById("minutes").innerHTML = minutes;
     document.getElementById("seconds").innerHTML = seconds;
 
-    // Stop countdown when it ends
-    
+    //Stop countdown and reset storage when done
+
     if (distance < 0) {
         clearInterval(x);
+        localStorage.removeItem("countDownDate");
         document.querySelector(".launch-time").innerHTML = "Launched!";
     }
 }, 1000);
